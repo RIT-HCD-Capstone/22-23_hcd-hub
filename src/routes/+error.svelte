212392b6267a41830dc2078797c24d26c268497a
jcleme <clemensjosh@protonmail.com>
2023-03-28 21:00:25 -0400

    error + layout pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center w-full h-screen">
+	<h1>404</h1>
+	<a href="/" class="p-12 px-16 bg-slate-500">Back To Home</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 </script>
 
-<main class="bg-slate-50 mx-4 h-screen rounded-lg">
+<!-- <main class="bg-slate-50 mx-4 h-screen rounded-lg"> -->
+<main>
+	<nav class="w-full bg-teal-400 h-14 flex flex-row items-center justify-start">
+		<a href="/" class="p-4 border-r-2">HCD Hub</a>
+		<!-- <a href="https://hcd-lab.student.rit.edu/hcd-hub/strapi/api/connect/google/callback"
+			>Login With Google</a
+		> -->
+	</nav>
 	<slot />
 </main>
