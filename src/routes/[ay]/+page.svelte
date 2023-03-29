c85356a74babfa44eec82e3dd3a4bb44d3cf3ec8
jcleme <clemensjosh@protonmail.com>
2023-03-28 21:01:45 -0400

    year overview page (not yet working)

--- src/routes/[ay]/+page.svelte
+++ src/routes/[ay]/+page.svelte
@@ -1,24 +1,6 @@
-<script context="module">
-	// /** @type {import('./$types').PageLoad} */
-	// export async function load({ fetch }) {
-	// 	const response = await fetch('http://localhost:1337/api/capstone-annual-years', {
-	// 		method: 'GET',
-	// 		headers: {
-	// 			Authorization:
-	// 				'bearer 90fd8bd73f326d2cabc81f528e2f4d67a9070ca8123a96e7e1a90c6c7df027483f32f885e8b48060bc0d9fa45cf20d4fc863897bcd4167a1f264bf21e6b80928dfd0eeb7faa649d929d9e25dfbe7637e23c8ce2f2c778483e47668cf44e4d51df9b4f3b9045d48425d539825596111f00f265355606870653d550e7c70b12c99'
-	// 		},
-	// 		mode: 'same-origin'
-	// 	});
-	// 	console.log(response);
-	// 	return { props: { data: response } };
-	// }
-</script>
-
-<script>
-	// /** @type {import('./$types').PageData} */
-	// export let data;
+<script lang="ts">
+	import { page } from '$app/stores';
 </script>
 
 <div>this page is not working lol</div>
-
-<!-- <div>{data}</div> -->
+<div>{$page.url}</div>
