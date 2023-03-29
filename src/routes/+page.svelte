85db0ec3ba88c357d839d00b4f458069b131484c
jcleme <clemensjosh@protonmail.com>
2023-03-28 21:01:05 -0400

    AllProjects page / project list page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
+	import ProjectCard from '$lib/ProjectCard.svelte';
+	import type { AllCapstoneProjects } from '$lib/ProjectTypes';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+	let projects = <AllCapstoneProjects>data.data;
+	console.log('projects: ' + projects);
+	console.log('projects.length: ' + projects.length);
+	console.log('projects.toString(): ' + projects.toString());
 </script>
 
 <svelte:head>
 	<title>HCD Hub</title>
 </svelte:head>
 
-<div class="my-4">
-	<h1 class="text-center text-3xl font-bold py-4">Humanities, Computing, and Design Hub</h1>
-	{#each data.data.data as project}
-		<div class="p-4 border border-red-500 mx-5 my-2">
-			<!-- <h1>{JSON.stringify(project.attributes.Title)}</h1> -->
-			<h1>{project.attributes.Title}</h1>
-			<p>{project.attributes.Description}</p>
-		</div>
-	{/each}
-</div>
+<main class="my-4">
+	<h1 class="text-3xl font-bold p-4">Humanities, Computing, and Design Hub</h1>
+	<!-- {data} -->
+	<section class="mx-4 grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-6">
+		{#each projects as project}
+			<ProjectCard
+				URL={project.attributes.annual_year.data.attributes.Year + '/' + project.id}
+				Logo={project.attributes.Logo?.data?.attributes?.url}
+				Title={project.attributes.Title}
+				Description={project.attributes.Description}
+				Annual_Year={project.attributes.annual_year.data.attributes.Year}
+				Num_PFRs={project.attributes.public_facing_reports?.data?.length}
+				Link={project.attributes.Link}
+				Num_Users={project.attributes.users?.data?.length}
+			/>
+		{/each}
+	</section>
+</main>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,39 @@
 import { json } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
+import type { AllCapstoneProjects } from "$lib/ProjectTypes";
+
+let data: AllCapstoneProjects;
 
 export const load = (async ({ fetch }) => {
   const res = await fetch(
-    "https://hcd-lab.student.rit.edu/hcd-hub/strapi/api/capstone-projects",
+    "https://hcd-lab.student.rit.edu/hcd-hub/strapi/api/capstone-projects?populate=*",
   );
-  const data = await res.json();
+  data = await res.json();
+  // const parsedData = JSON.stringify(returnedData);
+  console.log(data);
+  // for (let index = 0; index < returnedData.length; index++) {
+  //   const project = new CapstoneProject(returnedData[index]);
+  //   // const element = array[index];
+  //   data.push(project);
+  // }
+
+  // returnedData.forEach((item: CapstoneProject) => {
+  //   let project = {
+  //     id: item.id,
+  //     attributes: {
+  //       Title: item.attributes.Title,
+  //       Description: item.attributes.Description,
+  //       createdAt: item.attributes.createdAt,
+  //       updatedAt: item.attributes.updatedAt,
+  //       Logo: item.attributes.Logo,
+  //     },
+  //   };
+  //   data.push(project);
+  // });
+
+  return data;
+
+  // });
 
   return { data };
 }) satisfies PageLoad;
