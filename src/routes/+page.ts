6b76ac7ccca8bcc25febed9bcc761f2c4f5b62ce
jcleme <clemensjosh@protonmail.com>
2023-04-12 12:12:26 -0400

    sort project cards by AY date

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -9,6 +9,7 @@ export const load = (async ({ fetch }) => {
     "https://hcd-lab.student.rit.edu/hcd-hub/strapi/api/capstone-projects?populate=*",
   );
   data = await res.json();
-
+  data.data = data.data.sort((n1, n2) => parseInt(n2.attributes.annual_year.data.attributes.Year.substring(7, 9)) - parseInt(n1.attributes.annual_year.data.attributes.Year.substring(7, 9)));
   return data;
+
 }) satisfies PageLoad;
